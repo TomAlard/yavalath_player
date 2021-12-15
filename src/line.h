dbcf40a846d0ed4773aaf1415fb4d1816870c629
Tom Alard <tom.alard@ugent.be>
2021-12-15 10:54:35 +0100

    remove private function from header

--- src/line.h
+++ src/line.h
@@ -3,8 +3,6 @@
 
 #include "game.h"
 
-void get_line(Coord line[4], Coord start, uint8_t direction);
-
 void construct_line_through_coord(Coord coord, uint8_t direction, Coord line[7]);
 
 uint8_t calculate_line_length(Game* game, Coord start, uint8_t direction);
